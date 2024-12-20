02a3692a4ba7371e125a437fb660da9ec7be1082
kone <adaminfo.514675@gmail.com>
2024-12-20 17:21:03 +0000

    Updated HTML and JavaScript for new features

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById('welcomeButton').addEventListener('click', function() {
  document.getElementById('welcomeMessage').textContent = 'Nous sommes ravis de vous accueillir sur le site dédié à l\'EPO. Explorez nos programmes et nos initiatives!';
 });
+
+function changeDynamicContent() {
+ document.getElementById('dynamicContent').textContent = 'Le contenu a été changé
+grâce à JavaScript!';
+}
+window.onload = function() {
+ changeDynamicContent();
+};
