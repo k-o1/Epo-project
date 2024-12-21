a094cefdeadf96cdc1eacaf997998d5ea5c6cdb6
kone <adaminfo.514675@gmail.com>
2024-12-21 10:36:08 +0000

    Ajout d'une galerie d'images pour l'EPO

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ grâce à JavaScript!';
 window.onload = function() {
  changeDynamicContent();
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+ const images = document.querySelectorAll("#galerie img");
+ images.forEach(image => {
+ image.addEventListener("click", () => {
+ alert("Vous avez cliqué sur une image !");
+ });
+ });
+});
